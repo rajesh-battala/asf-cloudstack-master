7b94bf521ec2b2ff21c6db034623d4bf0d3357c7
Rajesh Battala <rajesh.battala@citrix.com>
2015-07-23 14:24:39 +0530

    Modified Netscaler Plugin to support Netscaler Control Center Changes

--- server/src/com/cloud/network/ExternalLoadBalancerDeviceManagerImpl.java
+++ server/src/com/cloud/network/ExternalLoadBalancerDeviceManagerImpl.java
@@ -1113,7 +1113,12 @@ public abstract class ExternalLoadBalancerDeviceManagerImpl extends AdapterBase 
         if (!(startup[0] instanceof StartupExternalLoadBalancerCommand)) {
             return null;
         }
-        host.setType(Host.Type.ExternalLoadBalancer);
+        if(host.getName().equalsIgnoreCase("NetScalerControlCenter")) {
+            host.setType(Host.Type.NetScalerControlCenter);
+        }
+        else {
+            host.setType(Host.Type.ExternalLoadBalancer);
+        }
         return host;
     }
 
